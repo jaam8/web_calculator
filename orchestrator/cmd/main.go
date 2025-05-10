e1e29850ec21c9dd46150860021d0bd2af4a6ecf
jaam8 <jaam8@yandex.ru>
2025-05-11 04:44:43 +0700

    faet(orchestrator): add setup to service

--- orchestrator/cmd/main.go
+++ orchestrator/cmd/main.go
@@ -1,11 +1,48 @@
 package main
 
 import (
-	"github.com/jaam8/web_calculator/internal/api"
-	"github.com/jaam8/web_calculator/internal/logger"
+	"context"
+	"github.com/jaam8/web_calculator/common-lib/logger"
+	"github.com/jaam8/web_calculator/orchestrator/internal/config"
+	"github.com/jaam8/web_calculator/orchestrator/internal/server"
+	"github.com/jaam8/web_calculator/orchestrator/internal/service/utils"
+	"go.uber.org/zap"
+	"log"
+	"os"
+	"os/signal"
 )
 
 func main() {
-	logger.InitLogger()
-	api.RunServer()
+	ctx, stop := signal.NotifyContext(context.Background(), os.Interrupt)
+	defer stop()
+	ctx, _ = logger.New(ctx)
+
+	cfg, err := config.New()
+	if err != nil {
+		logger.GetLoggerFromCtx(ctx).Fatal(ctx,
+			"failed to load config", zap.Error(err))
+	}
+
+	orchestratorCfg := cfg.Orchestrator
+
+	durations := map[string]int{
+		"+": orchestratorCfg.TimeAddition,
+		"-": orchestratorCfg.TimeSubtraction,
+		"*": orchestratorCfg.TimeMultiplications,
+		"/": orchestratorCfg.TimeDivisions,
+	}
+	expressionManager := utils.NewExpressionManager(durations)
+
+	Server := server.NewOrchestratorService(expressionManager)
+	grpcServer, err := server.CreateGRPC(Server)
+	if err != nil {
+		log.Fatalf("failed to create gRPC server: %v", err)
+	}
+
+	go server.RunGRPC(ctx, grpcServer, orchestratorCfg.Port)
+
+	<-ctx.Done()
+	grpcServer.GracefulStop()
+	stop()
+	logger.GetLoggerFromCtx(ctx).Info(ctx, "ORCHESTRATOR server stopped")
 }
