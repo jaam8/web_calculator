0c1c8853d3bd802f111fe54576c311cb4cde44aa
jaam8 <jaam8@yandex.ru>
2025-05-11 05:19:49 +0700

    feat(agent): add setup for agent service

--- agent/internal/server/server.go
+++ agent/internal/server/server.go
@@ -1,1 +1,21 @@
 package server
+
+import (
+	"github.com/jaam8/web_calculator/agent/internal/service"
+	"log"
+	"sync"
+)
+
+func RunAgentService(agentService *service.AgentService, computingPower int, waitTime int) {
+	var wg sync.WaitGroup
+
+	for i := 0; i < computingPower; i++ {
+		wg.Add(1)
+		log.Println("AGENT Starting worker ", i)
+		go func() {
+			defer wg.Done()
+			agentService.Work(waitTime)
+		}()
+	}
+	wg.Wait()
+}
