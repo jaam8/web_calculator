a4193e2b6fe0fb6f507c3709011c2d24c0ed9ccf
jaam8 <jaam8@yandex.ru>
2025-05-11 04:34:18 +0700

    faet(orchestrator): add setup for grpc

--- orchestrator/internal/server/server.go
+++ orchestrator/internal/server/server.go
@@ -1,1 +1,43 @@
 package server
+
+import (
+	"context"
+	"fmt"
+	"github.com/jaam8/web_calculator/common-lib/gen/orchestrator"
+	"github.com/jaam8/web_calculator/common-lib/grpc/interceptors"
+	"github.com/jaam8/web_calculator/common-lib/logger"
+	"github.com/jaam8/web_calculator/orchestrator/internal/service"
+	"github.com/jaam8/web_calculator/orchestrator/internal/service/utils"
+	"go.uber.org/zap"
+	"google.golang.org/grpc"
+	"net"
+)
+
+func CreateGRPC(grpcSrv *service.OrchestratorService) (*grpc.Server, error) {
+	server := grpc.NewServer(grpc.UnaryInterceptor(interceptors.AddLogMiddleware))
+	orchestrator.RegisterOrchestratorServiceServer(server, grpcSrv)
+	return server, nil
+}
+
+func NewOrchestratorService(
+	expressionManager *utils.ExpressionManager,
+) *service.OrchestratorService {
+	return service.NewOrchestratorService(expressionManager)
+}
+
+func RunGRPC(ctx context.Context, server *grpc.Server, port int) {
+	lis, err := net.Listen("tcp", fmt.Sprintf("0.0.0.0:%d", port))
+	if err != nil {
+		logger.GetLoggerFromCtx(ctx).Fatal(ctx,
+			"ORCHESTRATOR failed to create listener on port",
+			zap.Int("port", port),
+			zap.Error(err))
+	}
+
+	logger.GetLoggerFromCtx(ctx).Info(ctx, fmt.Sprintf("ORCHESTRATOR listening at :%d", port))
+	if err = server.Serve(lis); err != nil {
+		logger.GetLoggerFromCtx(ctx).Fatal(ctx,
+			"ORCHESTRATOR failed to serve grpc server",
+			zap.Error(err))
+	}
+}
