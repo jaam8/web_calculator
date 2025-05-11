694520388e358ea92ac13525ff45ee2a235f6bbc
jaam8 <jaam8@yandex.ru>
2025-05-12 03:48:26 +0700

    feat(orchestrator): add logging to service

--- orchestrator/internal/service/service.go
+++ orchestrator/internal/service/service.go
@@ -2,12 +2,15 @@ package service
 
 import (
 	"context"
+	"fmt"
 	"github.com/jaam8/web_calculator/common-lib/errors"
 	"github.com/jaam8/web_calculator/common-lib/gen/orchestrator"
+	"github.com/jaam8/web_calculator/common-lib/logger"
 	"github.com/jaam8/web_calculator/orchestrator/internal/models"
 	"github.com/jaam8/web_calculator/orchestrator/internal/service/helper"
 	"github.com/jaam8/web_calculator/orchestrator/internal/service/types"
 	"github.com/jaam8/web_calculator/orchestrator/internal/service/utils"
+	"go.uber.org/zap"
 	"google.golang.org/protobuf/types/known/durationpb"
 	"google.golang.org/protobuf/types/known/emptypb"
 )
@@ -27,10 +30,19 @@ func (s *OrchestratorService) Calculate(
 	ctx context.Context, request *orchestrator.CalculateRequest,
 ) (*orchestrator.CalculateResponse, error) {
 	rpn, err := helper.ToRPN(request.Expression)
+	logger.GetLoggerFromCtx(ctx).Debug(ctx,
+		fmt.Sprintf("RPN for expression: %s", request.Expression),
+		zap.Any("rpn", rpn),
+		zap.Error(err))
 	if err != nil {
 		return nil, err
 	}
 	expressionID, err := s.expressionManager.CreateExpression()
+	logger.GetLoggerFromCtx(ctx).Debug(ctx,
+		fmt.Sprintf("Created expression with id: %d", expressionID),
+		zap.Int("expressionID", expressionID),
+		zap.Error(err),
+	)
 	if err != nil {
 		return nil, err
 	}
@@ -55,15 +67,21 @@ func (s *OrchestratorService) Expressions(
 		}
 		exprs[i] = expr
 	}
+	logger.GetLoggerFromCtx(ctx).Info(ctx,
+		fmt.Sprintf("got %d expressions", len(expressions)),
+		zap.Int("expressionsCount", len(expressions)))
 	return &orchestrator.ExpressionsResponse{Expressions: exprs}, nil
 }
 
-func (s *OrchestratorService) ExpressionByID(
+func (s *OrchestratorService) ExpressionById(
 	ctx context.Context, request *orchestrator.ExpressionByIdRequest,
 ) (*orchestrator.ExpressionByIdResponse, error) {
 	expression, ok := s.expressionManager.GetExpression(int(request.Id))
 	if !ok {
-		// add norm error
+		logger.GetLoggerFromCtx(ctx).Warn(ctx,
+			fmt.Sprintf("no expression found for id: %d", request.Id),
+			zap.Int("expressionID", int(request.Id)),
+		)
 		return nil, errors.ErrExpressionNotFound
 	}
 	expr := &orchestrator.Expression{
@@ -71,17 +89,24 @@ func (s *OrchestratorService) ExpressionByID(
 		Status: expression.Status,
 		Result: expression.Result,
 	}
+	logger.GetLoggerFromCtx(ctx).Info(ctx,
+		"got expression by id",
+		zap.Int64("ExpressionID", expr.Id),
+		zap.String("status", expr.Status),
+		zap.Float64p("result", expr.Result))
 	return &orchestrator.ExpressionByIdResponse{Expression: expr}, nil
 }
 
 func (s *OrchestratorService) ResultTask(
 	ctx context.Context, request *orchestrator.ResultTaskRequest,
 ) (*orchestrator.ResultTaskResponse, error) {
-	// todo why task id and not expression id
 	taskManager, ok := s.expressionManager.GetTaskManager(int(request.ExpressionId))
 	if !ok {
+		logger.GetLoggerFromCtx(ctx).Debug(ctx,
+			fmt.Sprintf("no task manager found for expression with id: %d", request.ExpressionId),
+			zap.Int("expressionID", int(request.ExpressionId)),
+		)
 		return nil, errors.ErrTaskNotFound
-
 	}
 	result := models.Result{
 		ExpressionID: int(request.ExpressionId),
@@ -89,17 +114,33 @@ func (s *OrchestratorService) ResultTask(
 		Result:       request.Result,
 	}
 	taskManager.AddResult(result)
+	logger.GetLoggerFromCtx(ctx).Info(ctx,
+		fmt.Sprintf("got result for task with id: %d", request.Id),
+		zap.Int("expressionID", int(request.ExpressionId)),
+		zap.Int("taskID", int(request.Id)),
+		zap.Float64("result", request.Result),
+	)
 	return &orchestrator.ResultTaskResponse{Status: "task completed"}, nil
 }
 
 func (s *OrchestratorService) GetTask(
-	ctx context.Context, request *emptypb.Empty,
+	ctx context.Context, _ *emptypb.Empty,
 ) (*orchestrator.GetTaskResponse, error) {
 	select {
 	case task := <-s.expressionManager.GetTasks():
 		if task.TaskID != 0 {
+			logger.GetLoggerFromCtx(ctx).Debug(ctx,
+				fmt.Sprintf("send task with id: %d", task.TaskID),
+				zap.Int("expressionID", task.ExpressionID),
+				zap.Int("taskID", task.TaskID),
+				zap.Float64("arg1", task.Arg1),
+				zap.Float64("arg2", task.Arg2),
+				zap.String("operation", task.Operation),
+				zap.Duration("operationTime", task.OperationTime),
+			)
 			return &orchestrator.GetTaskResponse{
 				Task: &orchestrator.Task{
+					ExpressionId:  int64(task.ExpressionID),
 					Id:            int64(task.TaskID),
 					Arg1:          task.Arg1,
 					Arg2:          task.Arg2,
@@ -109,7 +150,12 @@ func (s *OrchestratorService) GetTask(
 			}, nil
 		}
 	default:
+		logger.GetLoggerFromCtx(ctx).Warn(ctx, "no task found")
 		return nil, errors.ErrTaskNotFound
 	}
+	logger.GetLoggerFromCtx(ctx).Error(ctx,
+		"internal server error",
+		zap.Error(errors.ErrInternalServerError),
+	)
 	return nil, errors.ErrInternalServerError
 }
--- orchestrator/internal/service/utils/processor.go
+++ orchestrator/internal/service/utils/processor.go
@@ -1,11 +1,17 @@
 package utils
 
 import (
+	"context"
+	"fmt"
+	"github.com/jaam8/web_calculator/common-lib/logger"
 	"github.com/jaam8/web_calculator/orchestrator/internal/service/types"
+	"go.uber.org/zap"
 	"strconv"
 )
 
 func Process(rpn []string, tm types.TaskManager, em types.ExpressionManager, expressionID int) {
+	ctx, _ := logger.New(context.Background())
+
 	var stack []float64
 	for _, v := range rpn {
 		if num, err := strconv.ParseFloat(v, 64); err == nil {
@@ -21,8 +27,20 @@ func Process(rpn []string, tm types.TaskManager, em types.ExpressionManager, exp
 		stack = stack[:len(stack)-2]
 
 		task := tm.CreateTask(arg1, arg2, v, expressionID)
+		logger.GetLoggerFromCtx(ctx).Debug(ctx,
+			fmt.Sprintf("created task with id: %d", task.TaskID),
+			zap.Int("expressionID", task.ExpressionID),
+			zap.Int("taskID", task.TaskID),
+			zap.Float64("arg1", task.Arg1),
+			zap.Float64("arg2", task.Arg2),
+			zap.String("operator", task.Operation))
 		em.AddTask(task)
 		result := tm.GetResult()
+		logger.GetLoggerFromCtx(ctx).Debug(ctx,
+			fmt.Sprintf("got result for task with id: %d", task.TaskID),
+			zap.Int("expressionID", expressionID),
+			zap.Int("taskID", task.TaskID),
+			zap.Float64("result", result.Result))
 		stack = append(stack, result.Result)
 	}
 
