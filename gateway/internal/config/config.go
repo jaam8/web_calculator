8e726ee5ae919a97c7265013cfa47a2394ea1be0
jaam8 <jaam8@yandex.ru>
2025-05-12 04:14:52 +0700

    feat(gateway): add config for all services

---
+++ gateway/internal/config/config.go
@@ -0,0 +1,51 @@
+package config
+
+import (
+	"fmt"
+	"github.com/ilyakaznacheev/cleanenv"
+)
+
+type OrchestratorConfig struct {
+	Host string `yaml:"host" env:"HOST" env-default:"localhost"`
+	Port int    `yaml:"port" env:"PORT" env-default:"50052"`
+
+	UpstreamName string `yaml:"upstream_name" env:"UPSTREAM_NAME" env-default:"orchestrator"`
+	UpstreamPort int    `yaml:"upstream_port" env:"UPSTREAM_PORT" env-default:"50052"`
+
+	Timeout        int  `yaml:"timeout" env:"TIMEOUT_MS" env-default:"500"`
+	MaxRetries     uint `yaml:"max_retries" env:"MAX_RETRIES" env-default:"3"`
+	BaseRetryDelay int  `yaml:"base_retry_delay" env:"BASE_RETRY_DELAY" env-default:"100"`
+}
+
+type GrpcPoolConfig struct {
+	MaxConns         int  `yaml:"max_conns" env:"MAX_CONNS" env-default:"10"`
+	MinConns         int  `yaml:"min_conns" env:"MIN_CONNS" env-default:"1"`
+	MaxRetries       uint `yaml:"max_retries" env:"MAX_RETRIES" env-default:"3"`
+	BaseRetryDelayMs uint `yaml:"base_retry_delay_ms" env:"BASE_RETRY_DELAY_MS" env-default:"200"`
+}
+
+type GatewayConfig struct {
+	Host string `yaml:"host" env:"HOST" env-default:"localhost"`
+	Port int    `yaml:"port" env:"PORT" env-default:"8080"`
+}
+
+type Config struct {
+	Orchestrator OrchestratorConfig `yaml:"orchestrator" env-prefix:"ORCHESTRATOR_"`
+	Gateway      GatewayConfig      `yaml:"gateway" env-prefix:"GATEWAY_"`
+	GrpcPool     GrpcPoolConfig     `yaml:"grpc_pool" env-prefix:"GRPC_POOL_"`
+	LogLevel     string             `yaml:"log_level" env:"LOG_LEVEL" env-default:"info"`
+}
+
+func New() (Config, error) {
+	var cfg Config
+	// docker workdir - app/
+	// local workdir - web_calculator/gateway
+	if err := cleanenv.ReadConfig("../.env", &cfg); err != nil {
+		fmt.Println(err.Error())
+		if err := cleanenv.ReadEnv(&cfg); err != nil {
+			return Config{}, fmt.Errorf("failed to read env vars: %v", err)
+		}
+	}
+
+	return cfg, nil
+}
