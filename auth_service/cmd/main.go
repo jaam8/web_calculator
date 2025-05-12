c5de778b8bed8de8965feab4a926597e1c0622b7
jaam8 <jaam8@yandex.ru>
2025-05-13 02:16:15 +0700

    feat(auth_service): add setup for all service

---
+++ auth_service/cmd/main.go
@@ -0,0 +1,67 @@
+package main
+
+import (
+	"context"
+	"github.com/jaam8/web_calculator/auth_service/internal/config"
+	"github.com/jaam8/web_calculator/auth_service/internal/ports/adapters/cache"
+	"github.com/jaam8/web_calculator/auth_service/internal/ports/adapters/storage"
+	"github.com/jaam8/web_calculator/auth_service/internal/server"
+	"github.com/jaam8/web_calculator/common-lib/logger"
+	"github.com/jaam8/web_calculator/common-lib/postgres"
+	"github.com/jaam8/web_calculator/common-lib/redis"
+	"log"
+	"os"
+	"os/signal"
+	"time"
+)
+
+func main() {
+	ctx, stop := signal.NotifyContext(context.Background(), os.Interrupt)
+	defer stop()
+
+	cfg, err := config.New()
+	if err != nil {
+		log.Fatalf("failed to load config: %v", err)
+	}
+
+	ctx = context.WithValue(ctx, "log_level", cfg.LogLevel)
+	ctx, _ = logger.New(ctx)
+
+	authCfg := cfg.AuthService
+	redisCfg := cfg.Redis
+	postgresCfg := cfg.Postgres
+
+	redisClient, err := redis.NewRedisClient(ctx, redisCfg, authCfg.RedisDB)
+	if err != nil {
+		log.Fatalf("failed to create redis client: %v", err)
+	}
+
+	PostgresClient, err := postgres.New(ctx, postgresCfg)
+	if err != nil {
+		log.Fatalf("failed to create postgres client: %v", err)
+	}
+
+	redisAdapter := cache.NewAuthCacheAdapter(redisClient,
+		time.Hour*time.Duration(cfg.RefreshExpiration),
+		time.Minute*time.Duration(cfg.AccessExpiration),
+	)
+
+	postgresAdapter := storage.NewAuthPostgresAdapter(PostgresClient)
+
+	Server := server.NewAuthService(postgresAdapter, redisAdapter, cfg.JwtSecret,
+		time.Hour*time.Duration(cfg.RefreshExpiration),
+		time.Minute*time.Duration(cfg.AccessExpiration),
+	)
+	
+	grpcServer, err := server.CreateGRPC(Server)
+	if err != nil {
+		log.Fatalf("failed to create gRPC server: %v", err)
+	}
+
+	go server.RunGRPC(ctx, grpcServer, authCfg.Port)
+
+	<-ctx.Done()
+	grpcServer.GracefulStop()
+	stop()
+	logger.GetLoggerFromCtx(ctx).Info(ctx, "AUTH_SERVICE server stopped")
+}
