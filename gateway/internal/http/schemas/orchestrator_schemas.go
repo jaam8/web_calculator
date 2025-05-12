39d464021e0abdb080f6b4f07bac553a9c717cee
jaam8 <jaam8@yandex.ru>
2025-05-13 02:29:29 +0700

    feat(gateway): add new orchestrator schemas for swagger

--- gateway/internal/http/schemas/orchestrator_schemas.go
+++ gateway/internal/http/schemas/orchestrator_schemas.go
@@ -1,18 +1,50 @@
 package schemas
 
 type CalculateRequest struct {
-	Expression string `json:"expression"`
+	Expression string `json:"expression" example:"40+2"`
+}
+
+type CalculateResponse struct {
+	Id int `json:"id" example:"1"`
 }
 
 type Expression struct {
-	Id     int      `json:"id"`
-	Status string   `json:"status"`
-	Result *float64 `json:"result,omitempty"`
+	Id     int      `json:"id" example:"1"`
+	Status string   `json:"status" example:"done"`
+	Result *float64 `json:"result,omitempty" example:"42.0"`
+}
+
+type ExpressionsResponse struct {
+	Expressions []Expression `json:"expressions"`
+}
+
+type ExpressionByIdRequest struct {
+	Id int64 `json:"id" example:"1"`
+}
+
+type ExpressionByIdResponse struct {
+	Expression
+}
+
+type InternalServerError struct {
+	Error string `json:"error" example:"internal server error"`
+}
+
+type ExpressionNotFound struct {
+	Error string `json:"error" example:"expression not found"`
+}
+
+type CannotParseId struct {
+	Error string `json:"error" example:"cannot parse id"`
+}
+
+type CannotParseExpression struct {
+	Error string `json:"error" example:"cannot parse expression"`
 }
 
 var (
-	InternalServerError   = "internal server error"
-	ExpressionNotFound    = "expression not found"
-	CannotParseId         = "cannot parse id"
-	CannotParseExpression = "cannot parse expression"
+	InternalServerErrorMsg   = InternalServerError{Error: "internal server error"}
+	ExpressionNotFoundMsg    = ExpressionNotFound{Error: "expression not found"}
+	CannotParseIdMsg         = CannotParseId{Error: "cannot parse id"}
+	CannotParseExpressionMsg = CannotParseExpression{Error: "cannot parse expression"}
 )
