968d87308b3148cf0bdd09f56818772395652a0c
jaam8 <jaam8@yandex.ru>
2025-05-14 05:19:50 +0700

    feat(auth_service): update configs for auth_service

--- gateway/internal/config/config.go
+++ gateway/internal/config/config.go
@@ -17,6 +17,16 @@ type OrchestratorConfig struct {
 	BaseRetryDelay int  `yaml:"base_retry_delay" env:"BASE_RETRY_DELAY" env-default:"100"`
 }
 
+type AuthConfig struct {
+	Host           string `yaml:"host" env:"HOST" env-default:"localhost"`
+	Port           int    `yaml:"port" env:"PORT" env-default:"50051"`
+	UpstreamName   string `yaml:"upstream_name" env:"UPSTREAM_NAME" env-default:"auth"`
+	UpstreamPort   int    `yaml:"upstream_port" env:"UPSTREAM_PORT" env-default:"50051"`
+	Timeout        int    `yaml:"timeout" env:"TIMEOUT_MS" env-default:"500"`
+	MaxRetries     uint   `yaml:"max_retries" env:"MAX_RETRIES" env-default:"3"`
+	BaseRetryDelay int    `yaml:"base_retry_delay" env:"BASE_RETRY_DELAY" env-default:"100"`
+}
+
 type GrpcPoolConfig struct {
 	MaxConns         int  `yaml:"max_conns" env:"MAX_CONNS" env-default:"10"`
 	MinConns         int  `yaml:"min_conns" env:"MIN_CONNS" env-default:"1"`
@@ -33,7 +43,11 @@ type Config struct {
 	Orchestrator OrchestratorConfig `yaml:"orchestrator" env-prefix:"ORCHESTRATOR_"`
 	Gateway      GatewayConfig      `yaml:"gateway" env-prefix:"GATEWAY_"`
 	GrpcPool     GrpcPoolConfig     `yaml:"grpc_pool" env-prefix:"GRPC_POOL_"`
+	AuthService  AuthConfig         `yaml:"auth_service" env-prefix:"AUTH_SERVICE_"`
 	LogLevel     string             `yaml:"log_level" env:"LOG_LEVEL" env-default:"info"`
+	JwtSecret    string             `yaml:"jwt_secret" env:"JWT_SECRET"`
+	AccessTTL    int                `yaml:"access_ttl" env:"ACCESS_EXPIRATION" env-default:"15"`
+	RefreshTTL   int                `yaml:"refresh_ttl" env:"REFRESH_EXPIRATION" env-default:"24"`
 }
 
 func New() (Config, error) {
