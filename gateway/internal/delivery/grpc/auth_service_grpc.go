bf65fd6ce962af1e30a4c73df9bf964da165b0a2
jaam8 <jaam8@yandex.ru>
2025-05-14 05:16:21 +0700

    feat(gateway): implement grpc handlers for auth service

---
+++ gateway/internal/delivery/grpc/auth_service_grpc.go
@@ -0,0 +1,90 @@
+package grpc
+
+import (
+	"fmt"
+	"github.com/jaam8/web_calculator/common-lib/callers"
+	auth "github.com/jaam8/web_calculator/common-lib/gen/auth_service"
+	"github.com/jaam8/web_calculator/gateway/internal/ports"
+	"time"
+)
+
+type AuthService struct {
+	authAdapter *ports.AuthServiceAdapter
+	MaxRetries  uint
+	BaseDelay   time.Duration
+}
+
+func NewAuthService(authAdapter ports.AuthServiceAdapter,
+	maxRetries uint, baseDelay time.Duration) *AuthService {
+	return &AuthService{
+		authAdapter: &authAdapter,
+		MaxRetries:  maxRetries,
+		BaseDelay:   baseDelay,
+	}
+}
+
+func (s *AuthService) Login(request *auth.LoginRequest) (*auth.LoginResponse, error) {
+	resultChan := make(chan *auth.LoginResponse, 1)
+
+	err := callers.Retry(func() error {
+		response, err := (*s.authAdapter).Login(request)
+		if err != nil {
+			return fmt.Errorf("error in retry Login caller: %v", err)
+		}
+		resultChan <- response
+		return nil
+	}, s.MaxRetries, s.BaseDelay)
+
+	if err != nil {
+		return nil, fmt.Errorf("couldn't call Login: %v", err)
+	}
+
+	response := <-resultChan
+	close(resultChan)
+
+	return response, nil
+}
+
+func (s *AuthService) Register(request *auth.RegisterRequest) (*auth.RegisterResponse, error) {
+	resultChan := make(chan *auth.RegisterResponse, 1)
+
+	err := callers.Retry(func() error {
+		response, err := (*s.authAdapter).Register(request)
+		if err != nil {
+			return fmt.Errorf("error in retry Register caller: %v", err)
+		}
+		resultChan <- response
+		return nil
+	}, s.MaxRetries, s.BaseDelay)
+
+	if err != nil {
+		return nil, fmt.Errorf("couldn't call Register: %v", err)
+	}
+
+	response := <-resultChan
+	close(resultChan)
+
+	return response, nil
+}
+
+func (s *AuthService) Refresh(request *auth.RefreshRequest) (*auth.RefreshResponse, error) {
+	resultChan := make(chan *auth.RefreshResponse, 1)
+
+	err := callers.Retry(func() error {
+		response, err := (*s.authAdapter).Refresh(request)
+		if err != nil {
+			return fmt.Errorf("error in retry Refresh caller: %v", err)
+		}
+		resultChan <- response
+		return nil
+	}, s.MaxRetries, s.BaseDelay)
+
+	if err != nil {
+		return nil, fmt.Errorf("couldn't call Refresh: %v", err)
+	}
+
+	response := <-resultChan
+	close(resultChan)
+
+	return response, nil
+}
