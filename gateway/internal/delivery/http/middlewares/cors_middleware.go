13c1e3c34bb1e3e4330796a00d62e547d8be9832
jaam8 <jaam8@yandex.ru>
2025-05-16 00:58:23 +0700

    feat(gateway): update cors middleware

--- gateway/internal/delivery/http/middlewares/cors_middleware.go
+++ gateway/internal/delivery/http/middlewares/cors_middleware.go
@@ -7,9 +7,22 @@ import (
 
 func CORSMiddleware(next echo.HandlerFunc) echo.HandlerFunc {
 	return func(c echo.Context) error {
-		c.Response().Header().Set("Access-Control-Allow-Origin", "*")
+		allowedOrigins := []string{
+			"http://localhost:8081", // фронтенд
+			"http://frontend:8081",  // внутри Docker
+		}
+		origin := c.Request().Header.Get("Origin")
+		for _, allowed := range allowedOrigins {
+			if origin == allowed {
+				c.Response().Header().Set("Access-Control-Allow-Origin", allowed)
+				break
+			}
+		}
+
+		//c.Response().Header().Set("Access-Control-Allow-Origin", "*")
 		c.Response().Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
-		c.Response().Header().Set("Access-Control-Allow-Headers", "Content-Type")
+		c.Response().Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
+		c.Response().Header().Set("Access-Control-Allow-Credentials", "true")
 
 		if c.Request().Method == http.MethodOptions {
 			return c.NoContent(http.StatusNoContent)
